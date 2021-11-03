1291a2cec9aaa275360bcebca3552085d07eae72
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-03 00:00:19 -0400

    checkCarPayload middleware working

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,73 @@
+const Cars = require("./cars-model");
+const vinValidator = require("vin-validator");
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { id } = req.params;
+  Cars.getById(id)
+    .then((car) => {
+      if (!car) {
+        next({
+          sucess: false,
+          status: 404,
+          message: `car with id ${id} is not found`,
+        });
+      } else {
+        req.car = car;
+        next();
+      }
+    })
+    .catch(next);
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  // const error = { status : 400 }
+  if (!req.body.vin)
+    return next({
+      success: false,
+      status: 400,
+      message: "vin is missing",
+    });
+  if (!req.body.make)
+    return next({
+      success: false,
+      status: 400,
+      message: "make is missing",
+    });
+  if (!req.body.model)
+    return next({
+      success: false,
+      status: 400,
+      message: "model is missing",
+    });
+  if (!req.body.mileage)
+    return next({
+      success: false,
+      status: 400,
+      message: "mileage is missing",
+    });
+  next();
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin } = req.body;
+  if (vinValidator.validate(vin)) {
+    next();
+  } else {
+    next({
+      success: false,
+      status: 400,
+      message: `vin ${vin} is invalid`,
+    });
+  }
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
