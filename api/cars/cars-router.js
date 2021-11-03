b8bc784e210e23b6555bbf27f8fa0a2b57131280
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-03 00:20:26 -0400

    POST: endpoint created and working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Cars = require("./cars-model");
 const {
   checkCarId,
-  checkPayload,
+  checkCarPayload,
   checkVinNumberUnique,
   checkVinNumberValid,
 } = require("./cars-middleware");
@@ -17,6 +17,29 @@ router.get("/", (req, res, next) => {
 
 router.get("/:id", checkCarId, (req, res) => {
   res.status(200).json(req.carFromDb);
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    try {
+      const newCar = await Cars.create(req.body);
+      res.status(201).json(newCar);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 module.exports = router;
