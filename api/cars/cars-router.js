66503a821a79a8078b333193fc53ab5fe4161c5a
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-03 00:09:08 -0400

    GET: endpoint working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,16 @@
-// DO YOUR MAGIC
+const router = require("express").Router();
+const Cars = require("./cars-model");
+const {
+  checkCarId,
+  checkPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require("./cars-middleware");
+
+router.get("/", (req, res, next) => {
+  Cars.getAll(req.params.id)
+    .then((cars) => {
+      res.status(200).json(cars);
+    })
+    .catch(next);
+});
