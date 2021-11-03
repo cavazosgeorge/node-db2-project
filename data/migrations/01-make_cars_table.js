b07878fd347c6d994abdd65a5b76b7206afa5345
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-03 00:39:10 -0400

    updated async await function bug

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,4 +1,4 @@
-exports.up = function (knex) {
+exports.up = async function (knex) {
   await knex.schema.createTable("cars", (table) => {
     table.increments();
     table.text("vin").unique().notNullable();
@@ -10,6 +10,6 @@ exports.up = function (knex) {
   });
 };
 
-exports.down = function (knex) {
+exports.down = async function (knex) {
   await knex.schema.dropTableIfExists("cars");
 };
